6fb5cb9fc7d60a2fab6f8c47ba13f32e87df217e
Rahul-Das-Sarma <rahuldas35748@gmail.com>
2024-08-20 21:08:15 +0530

    chore: add techskill banner

--- src/components/marquee.tsx
+++ src/components/marquee.tsx
@@ -1,59 +1,62 @@
 import { cn } from "@/lib/utils";
 import Marquee from "@/components/magicui/marquee";
+import {
+  SiRedux,
+  SiReact,
+  SiApachekafka,
+  SiJavascript,
+  SiExpress,
+  SiGithub,
+} from "react-icons/si";
+import { IoLogoDocker } from "react-icons/io5";
+import { RiNextjsFill } from "react-icons/ri";
+import { SiKubernetes } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
 
 const reviews = [
   {
-    name: "Jack",
-    username: "@jack",
-    body: "I've never seen anything like this before. It's amazing. I love it.",
-    img: "https://avatar.vercel.sh/jack",
+    name: "Redux",
+    img: <SiRedux size={50} color="#764abc" />,
   },
   {
-    name: "Jill",
-    username: "@jill",
-    body: "I don't know what to say. I'm speechless. This is amazing.",
-    img: "https://avatar.vercel.sh/jill",
+    name: "React",
+    img: <SiReact size={50} color="#61dbfb" />,
   },
   {
-    name: "John",
-    username: "@john",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/john",
+    name: "Kafka",
+    img: <SiApachekafka size={50} />,
   },
   {
-    name: "Jane",
-    username: "@jane",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jane",
+    name: "Javascript",
+    img: <SiJavascript color="#F0DB4F" size={50} />,
   },
   {
-    name: "Jenny",
-    username: "@jenny",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jenny",
+    name: "Express",
+    img: <SiExpress color="green" size={50} />,
   },
   {
-    name: "James",
-    username: "@james",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/james",
+    name: "Docker",
+    img: <IoLogoDocker color="#0db7ed" size={50} />,
+  },
+  { name: "Next JS", img: <RiNextjsFill size={50} /> },
+  {
+    name: "Kubernetes",
+    img: <SiKubernetes size={50} />,
+  },
+  {
+    name: "Mongodb",
+    img: <SiMongodb color="green" size={50} />,
+  },
+  {
+    name: "Github",
+    img: <SiGithub size={50} />,
   },
 ];
 
 const firstRow = reviews.slice(0, reviews.length / 2);
 const secondRow = reviews.slice(reviews.length / 2);
 
-const ReviewCard = ({
-  img,
-  name,
-  username,
-  body,
-}: {
-  img: string;
-  name: string;
-  username: string;
-  body: string;
-}) => {
+const ReviewCard = ({ img, name }: { img: React.ReactNode; name: string }) => {
   return (
     <figure
       className={cn(
@@ -64,32 +67,30 @@ const ReviewCard = ({
         "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
       )}
     >
-      <div className="flex flex-row items-center gap-2">
-        <img className="rounded-full" width="32" height="32" alt="" src={img} />
+      <div className="flex flex-col items-center gap-2">
+        {img}
         <div className="flex flex-col">
           <figcaption className="text-sm font-medium dark:text-white">
             {name}
           </figcaption>
-          <p className="text-xs font-medium dark:text-white/40">{username}</p>
         </div>
       </div>
-      <blockquote className="mt-2 text-sm">{body}</blockquote>
     </figure>
   );
 };
 
 export function MarqueeDemo() {
   return (
-    <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border  md:shadow-xl">
+    <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border ">
       <p className="text-3xl font-semibold mb-3">Tech Skills</p>
       <Marquee pauseOnHover className="[--duration:20s]">
         {firstRow.map((review) => (
-          <ReviewCard key={review.username} {...review} />
+          <ReviewCard key={review.name} {...review} />
         ))}
       </Marquee>
       <Marquee reverse pauseOnHover className="[--duration:20s]">
         {secondRow.map((review) => (
-          <ReviewCard key={review.username} {...review} />
+          <ReviewCard key={review.name} {...review} />
         ))}
       </Marquee>
       {/* <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
